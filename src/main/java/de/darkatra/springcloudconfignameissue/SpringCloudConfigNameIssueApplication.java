508e1bb7d542cf1de108a97243a2d3dbe84008e8
Tommy Schmidt <tommy.schmidt@idealo.de>
2021-01-08 11:21:40 +0100

    initial commit

---
+++ src/main/java/de/darkatra/springcloudconfignameissue/SpringCloudConfigNameIssueApplication.java
@@ -0,0 +1,13 @@
+package de.darkatra.springcloudconfignameissue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigNameIssueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudConfigNameIssueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/darkatra/springcloudconfignameissue/SpringCloudConfigNameIssueApplicationTests.java
@@ -0,0 +1,13 @@
+package de.darkatra.springcloudconfignameissue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigNameIssueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
